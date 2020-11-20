42c1ef9c41fec94a4a5cb20614f74e357f110a95
Thanathonster <73691152+Thanathonster@users.noreply.github.com>
2020-11-20 23:30:57 +0700

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'labitf4.mysql.database.azure.com' => 'labitf4.mysql.database.azure.com',
+    'win@labitf4' => 'win@labitf4',
+    'super0951942413_' => 'super0951942413_',
+    'labitf4' => 'labitf4',
+);
+?>
